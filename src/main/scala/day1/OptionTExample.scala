a67898fbbdee1d96670b55bcb7c87d5879e92273
s10a137g <s10a137g@gmail.com>
2025-11-09 19:49:38 +0900

    feat(day1): OptionTを使わない場合を追加

--- src/main/scala/day1/OptionTExample.scala
+++ src/main/scala/day1/OptionTExample.scala
@@ -9,6 +9,23 @@ import scala.concurrent.duration.DurationInt
 
 object OptionTExample extends App {
 
+  // OptionTを使わない場合
+  private val notOptionTResult1: Future[Option[Int]] = Future.successful(Some(20))
+  private val notOptionTResult2: Future[Option[Int]] = Future.successful(Some(30))
+
+  private val combinedNotOptionT: Future[Option[Int]] = for {
+    aOpt <- notOptionTResult1
+    bOpt <- notOptionTResult2
+  } yield for {
+    a <- aOpt
+    b <- bOpt
+  } yield a + b
+
+  private val notOptionTValue = Await.result(combinedNotOptionT, 1.second)
+
+  println(s"Not OptionT Result: $notOptionTValue") // Should print: Not OptionT Result: Some(50)
+
+  // OptionTを使う場合
   private val result1 = OptionT[Future, Int](Future(Some(10)))
   private val result2 = OptionT[Future, Int](Future(Some(5)))
 
