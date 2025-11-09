8630b34db019bb268da230001d13a080e2a5f72e
s10a137g <s10a137g@gmail.com>
2025-11-09 19:35:32 +0900

    feat(day1): add OptionTExample demonstrating OptionT usage with Future

---
+++ src/main/scala/day1/OptionTExample.scala
@@ -0,0 +1,22 @@
+package day1
+
+import cats.data.OptionT
+import cats.implicits._
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+
+object OptionTExample extends App {
+
+  private val result1 = OptionT[Future, Int](Future(Some(10)))
+  private val result2 = OptionT[Future, Int](Future(Some(5)))
+
+  private val combined: OptionT[Future, Int] = for {
+    a <- result1
+    b <- result2
+  } yield a + b
+
+  val value = Await.result(combined.value, 1.second)
+  println(s"Result: $value") // Should print: Result: Some(15)
+}
